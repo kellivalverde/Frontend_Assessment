a36c4182ed8e188c7df80e4b762a6bb9257cfd0a
kellivalverde <paughka@gmail.comls>
2020-07-23 16:31:04 -0400

    round 2 - fix4 nav

--- files/js/nav.js
+++ files/js/nav.js
@@ -1,8 +1,9 @@
-function myNavBarFunction() {
-    var x = document.getElementById("myNavBar");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
+/* Toggles between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
